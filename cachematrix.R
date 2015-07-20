e895c3399c9e7052c3943862537a1de9777e4ae8
lucthibeault <thibeaultluc@gmail.com>
2015-07-20 15:16:46 -0400

    Version 1 of the programming assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,61 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Pair of functions used to manage data caching for matrix inversion operations
+## The assumption is made that the matrix is always a square invertible matrix
 
-## Write a short comment describing this function
+## The functions are "makeCacheMatrix" to prepare the caching object and 
+## "cacheSolve" to resolve and cache the operation. If the operation has been
+## executed in the past, the cached result will be returned
 
-makeCacheMatrix <- function(x = matrix()) {
-
+## Creates an "cachable matrix" object for the inversion operation.
+## Parameters: 
+##              currMatrix: Matrix to be inverted. Default value: Empty matrix
+makeCacheMatrix <- function(currMatrix = matrix()) {
+    ## Cached result of the last computation
+    result <- NULL
+    
+    ## Sets the matrix to be computed and clears the cached result
+    set <- function (newMatrix)
+    {
+        currMatrix <<- newMatrix
+        result <<- NULL
+    }
+    
+    ## Gets the current matrix to be computed
+    get <- function ()
+    {
+        currMatrix
+    }
+    
+    ## Sets the cached result
+    setResult <- function (newResult)
+    {
+        result <<- newResult
+    }
+    
+    ## Gets the cached result
+    getResult <- function ()
+    {
+        result
+    }
+    
+    list(set = set, get = get, setResult = setResult, getResult = getResult)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Returns the inverted matrix of the specified matrix
+## Parametrs:
+##              cacheMatrix: Cached Matrix object, as returned by the
+##                           "makeCacheMatrix" function
+cacheSolve <- function(cacheMatrix, ...) {
+    cacheRes <- cacheMatrix$getResult()
+    
+    if (!is.null(cacheRes))
+    {
+        message("Getting cached data")
+        return(cacheRes)
+    }
+    
+    dataExec <- cacheMatrix$get()
+    cacheRes <- solve(dataExec, ...)
+    
+    cacheMatrix$setResult(cacheRes)
+    cacheRes
 }
